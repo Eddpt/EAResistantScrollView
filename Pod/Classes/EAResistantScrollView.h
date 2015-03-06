660d4f26e231329b10be1199390b64bb613f1001
Eddpt <Eddpt@users.noreply.github.com>
2015-03-06 14:37:57 +0000

    Fixed typos

--- Pod/Classes/EAResistantScrollView.h
+++ Pod/Classes/EAResistantScrollView.h
@@ -13,7 +13,7 @@ IB_DESIGNABLE
 
 /**
  *  This flag defines whether or not resistance is enabled on the scroll view.
- *  If this is set to NO, both @c resistanceFactor and @c resistanceProgressionRation
+ *  If this is set to NO, both @c resistanceFactor and @c resistanceProgressionRatio
  *  remain unused. Otherwise, if set to YES, resistance is applied based on those two values.
  */
 @property (nonatomic, assign) IBInspectable BOOL shouldApplyResistance;
@@ -21,12 +21,12 @@ IB_DESIGNABLE
 /**
  *  This property defines the base value of how resistant the scroll view is when
  *  performing overscroll operations. Please look at @c resistanceProgressionRatio
- *  in order to achieve progressive resistance during a overscroll operation.
+ *  in order to achieve progressive resistance during an overscroll operation.
  */
 @property (nonatomic, assign) IBInspectable float resistanceFactor;
 
 /**
- *  This property affect how much more resistant the scroll view becomes as the user
+ *  This property affects how much more resistant the scroll view becomes as the user
  *  continues performing overscroll operations. If this value is 0 the resistance will
  *  be defined solely by @c resistanceFactor and it will remain the same throughout
  *  the overscroll operation; otherwise, it will impact how much resistance is seen
@@ -37,7 +37,7 @@ IB_DESIGNABLE
 @property (nonatomic, assign) IBInspectable float resistanceProgressionRatio;
 
 /**
- *  If defined, this block is ran every time the scroll view's delegate method
+ *  If defined, this block is run every time the scroll view's delegate method
  *  @c scrollViewDidScroll: is called. Since this class sets itself as the scroll 
  *  view's delegate, this may be used when external code needs to be executed.
  */
